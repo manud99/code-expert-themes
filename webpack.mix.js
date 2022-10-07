a2b2b61826bcc3d998fba4f8c78a1dea330a0e55
Manuel Dublanc <manuel.dublanc@gmail.com>
2022-10-07 19:52:40 +0200

    Disable mix-manifest.json

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,26 +1,20 @@
 const mix = require('laravel-mix');
 const fs = require('fs');
 
-const customThemes = [
-    'vscode_dark.css',
-];
+const customThemes = ['vscode_dark.css'];
 
 function getFiles(dir) {
-    return fs.readdirSync(dir).filter(file => {
+    return fs.readdirSync(dir).filter((file) => {
         return fs.statSync(`${dir}/${file}`).isFile();
     });
-};
+}
 
 getFiles('postcss/themes/theme/').forEach((filepath) => {
     mix.postCss('postcss/themes/theme/' + filepath, 'themes/');
 });
 
 getFiles('postcss/custom').forEach((filepath) => {
-    mix.postCss('postcss/custom/' + filepath, 'themes/', [
-        require('postcss-nesting'),
-    ]);
+    mix.postCss('postcss/custom/' + filepath, 'themes/', [require('postcss-nesting')]);
 });
 
-mix
-    .setPublicPath('dist')
-    .disableNotifications();
+mix.setPublicPath('dist').disableNotifications().options({ manifest: false });
