1b72179ef1db6e93d9b2ad57f6fca1e16f7d72c5
Manuel Dublanc <manuel.dublanc@gmail.com>
2022-10-07 19:36:52 +0200

    Add prettier.

--- dist/background.js
+++ dist/background.js
@@ -1,42 +1,45 @@
 const DEFAULT_THEME = 'merbivore';
 const styles = [];
 
-function onTabLoaded(tabId, changeInfo) {
-    if (changeInfo.status != 'complete') {
+function onTabLoaded(tabId, changeInfo, tab) {
+    if (!tab.url?.startsWith('https://expert.ethz.ch/ide2') || changeInfo?.status != 'complete') {
         return;
     }
 
-    chrome.storage.sync.get({ theme: DEFAULT_THEME })
-        .then((items) => insertCSS(items.theme, tabId));
-
+    // Load the current theme and insert the corresponding stylesheet.
+    chrome.storage.sync.get({ theme: DEFAULT_THEME }).then((items) => insertCSS(items.theme, tabId));
 }
 
-function updateCSS(changes, area) {
+function updateTabs(changes, area) {
     if (area != 'sync' || !changes.theme?.newValue) {
         return;
     }
 
+    // Remove all inserted stylesheets.
     styles.forEach((injection) => chrome.scripting.removeCSS(injection));
 
-    chrome.tabs.query({ active: true })
-        .then((tab) => {
-            if (!tab) {
-                return;
-            }
+    // Insert newly set stylesheet in all active tabs.
+    chrome.tabs.query({ active: true }).then((tabs) => {
+        if (!tabs) {
+            return;
+        }
 
-            tab.forEach((tab) => insertCSS(changes.theme?.newValue, tab.id));
-        });
+        tabs.forEach((tab) => insertCSS(changes.theme?.newValue, tab.id));
+    });
 }
 
 function insertCSS(theme, tabId) {
+    // Create the CSSInjection object
     const injection = {
         files: [`/themes/${theme}.css`],
         target: { tabId: tabId },
     };
 
+    // Insert CSS to all Code Expert tabs.
     chrome.scripting.insertCSS(injection);
+    // Save a copy of CSSInjection for possible later removal.
     styles.push(injection);
 }
 
 chrome.tabs.onUpdated.addListener(onTabLoaded);
-chrome.storage.onChanged.addListener(updateCSS);
+chrome.storage.onChanged.addListener(updateTabs);
--- dist/popup.js
+++ dist/popup.js
@@ -1,16 +1,18 @@
 const DEFAULT_THEME = 'merbivore';
 
-function restore_options() {
+function getTheme() {
+    // Load current theme from storage.
     chrome.storage.sync.get({ theme: DEFAULT_THEME }, (items) => {
         document.getElementById('theme').value = items.theme;
     });
 }
 
 function saveTheme(event) {
+    // Store selected theme.
     chrome.storage.sync.set({
         theme: event.target.value,
     });
 }
 
-document.addEventListener('DOMContentLoaded', restore_options);
+document.addEventListener('DOMContentLoaded', getTheme);
 document.getElementById('theme').addEventListener('change', saveTheme);
